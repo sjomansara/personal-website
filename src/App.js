d53fa2661524284ce1cd3f156e3a1e70fedc5c6e
sjomansara <sara.sjoman@hotmail.com>
2022-02-03 13:51:37 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import HomePage from './pages/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-     
+    <HomePage></HomePage>
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import MyNavbar from "../components/MyNavbar"
+
+const HomePage = () => {
+    return (
+    <MyNavbar/>
+    )
+}
+
+export default HomePage
