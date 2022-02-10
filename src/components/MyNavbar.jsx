baf3d31e5bb2d0affb2ed7b63ed432ea3c52aba1
sjomansara <sara.sjoman@hotmail.com>
2022-02-10 11:58:13 +0100

    styled navbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,21 +1,15 @@
-import { Navbar, Nav, NavDropdown } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 
 const MyNavbar = () => {
     return ( 
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+        <Nav className="ml-auto">
+            <Nav.Link href="#home" className="navLink">HOME</Nav.Link>
+            <Nav.Link href="#link" className="navLink">ABOUT ME</Nav.Link>
+            <Nav.Link href="#link" className="navLink">PORTFOLIO</Nav.Link>
+            <Nav.Link href="#link" className="navLink">CONTACT</Nav.Link>
         </Nav>
         </Navbar.Collapse>
         </Navbar>
