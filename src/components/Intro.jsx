a33b1da302daf3c19202a2b86582c222ac469552
sjomansara <sara.sjoman@hotmail.com>
2022-02-11 21:44:34 +0100

    changed photo

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,14 +6,14 @@ import { faHand } from '@fortawesome/free-solid-svg-icons'
 
 const Intro = () => {
     return ( 
-        <Jumbotron className="ml-5" style={{ backgroundColor: "rgb(47, 47, 47)", color: "rgb(232, 225, 239)", marginTop: "130px"}}>
+        <Jumbotron className="ml-5" style={{ backgroundColor: "rgb(47, 47, 47)", color: "rgb(212, 196, 233)", marginTop: "130px"}}>
         <h4>Hi there! <FontAwesomeIcon icon={faHand}/></h4>
         <h1 className="mt-4 mb-3">I'm <span style={{ fontWeight: "bold" }}>Sara Sjöman</span></h1>
         <p>
             I'm a frontend developer who is located in Munich, Germany and <br></br> is passionate about clean and functional design.
         </p>
         <p>
-            <Button className="my-4 px-4 badge-pill" variant="outline-primary" style={{ color: "rgb(232, 225, 239)", borderColor: "rgb(232, 225, 239)" }}>Learn more about me</Button>
+            <Button className="my-4 px-4 badge-pill" variant="outline-primary" style={{ color: "rgb(212, 196, 233)", borderColor: "rgb(212, 196, 233)" }}>Learn more about me</Button>
         </p>
         {/* <p style={{ marginTop: "250px", marginLeft: "600px" }} >
             <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: "30px" }}/><FontAwesomeIcon className="ml-3" icon={faGithub} style={{ fontSize: "30px" }}/><FontAwesomeIcon className="ml-3" icon={faEnvelope} style={{ fontSize: "30px" }}/>
--- src/components/MyPhoto.jsx
+++ src/components/MyPhoto.jsx
@@ -1,6 +1,7 @@
 const MyPhoto = () => {
     return ( 
-        <img style={{ borderRadius: "50%", marginTop: "100px" }} src="https://avatars.githubusercontent.com/u/86473615?v=4" />
+        // <img style={{ borderRadius: "50%", marginTop: "100px" }} src="https://avatars.githubusercontent.com/u/86473615?v=4" />
+        <img className="myPhoto" src="Untitled design (1).png" />
     )
 }
 
