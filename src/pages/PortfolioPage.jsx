30e17f03437d2173577ccddfe883383f002e85b6
sjomansara <sara.sjoman@hotmail.com>
2022-02-15 22:04:38 +0100

    added images and overlay

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -10,36 +10,44 @@ const PortfolioPage = () => {
         <div className="cardContainer">
         <Row className="mx-5">
             <Col md={3}>
-                <Card className="portfolioCard" style={{ width: '18rem' }}>
-                    <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
-                        <Card.Body>
-                            <Card.Title><a href="https://github.com/sjomansara/strive-capstoneproject-spotifystats">Spotify Stats App</a></Card.Title>
-                        </Card.Body>
+                <Card className="portfolioCard hvr-grow" style={{ width: '18rem' }}>
+                    <div className="imgContainer">
+                    <Card.Img className="cardImg" variant="top" src="/SpotifyStats(8).png" />
+                    <div class="overlay">
+                        <div class="text">Spotify Stats App</div>
+                    </div>
+                    </div>
                 </Card>
             </Col>
             <Col md={3}>
-                <Card className="portfolioCard" style={{ width: '18rem' }}>
-                        <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                            </Card.Body>
+                <Card className="portfolioCard hvr-grow" style={{ width: '18rem' }}>
+                    <div className="imgContainer">
+                    <Card.Img className="cardImg" variant="top" src="/SpotifyStats(8).png" />
+                    <div class="overlay">
+                        <div class="text">Spotify Stats App</div>
+                    </div>
+                    </div>
                 </Card>
             </Col>
             <Col md={3}>
-                <Card className="portfolioCard" style={{ width: '18rem' }}>
-                        <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                            </Card.Body>
-                    </Card>
+                <Card className="portfolioCard hvr-grow" style={{ width: '18rem' }}>
+                    <div className="imgContainer">
+                    <Card.Img className="cardImg" variant="top" src="/SpotifyStats(8).png" />
+                    <div class="overlay">
+                        <div class="text">Spotify Stats App</div>
+                    </div>
+                    </div>
+                </Card>
             </Col>
             <Col md={3}>
-                <Card className="portfolioCard" style={{ width: '18rem' }}>
-                        <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                            </Card.Body>
-                    </Card>
+                <Card className="portfolioCard hvr-grow" style={{ width: '18rem' }}>
+                    <div className="imgContainer">
+                    <Card.Img className="cardImg" variant="top" src="/SpotifyStats(8).png" />
+                    <div class="overlay">
+                        <div class="text">Spotify Stats App</div>
+                    </div>
+                    </div>
+                </Card>
             </Col>
         </Row>
         </div>
