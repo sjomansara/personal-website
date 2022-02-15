6b354b81db75075214e9e91b43b4282eee5f85cd
sjomansara <sara.sjoman@hotmail.com>
2022-02-15 20:52:55 +0100

    added cards

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,17 +1,57 @@
-import { Col, Row } from "react-bootstrap"
+import { Col, Row, Card} from "react-bootstrap"
 
 const PortfolioPage = () => {
     return (
-        <div>
-        <Row>
-        <Col md={3}>
-        </Col>
-        <Col md={3}>
-        </Col>
-        <Col md={3}>
-        </Col>
-        <Col md={3}>
-        </Col>
+        <div className="cardContainer">
+        <Row className="mx-5" style={{ marginTop: "80px" }}>
+            <Col md={3}>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                </Card>
+            </Col>
+            <Col md={3}>
+                <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                </Card>
+            </Col>
+            <Col md={3}>
+                <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                    </Card>
+            </Col>
+            <Col md={3}>
+                <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                    </Card>
+            </Col>
         </Row>
         </div>
     )
