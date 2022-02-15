c9f8696329815528a0ada04e70b7877965e9b292
sjomansara <sara.sjoman@hotmail.com>
2022-02-15 15:02:21 +0100

    added animations to skills and added more text to about me

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -6,11 +6,11 @@ const MySkills = () => {
     return (
         <Jumbotron className="ml-5" style={{ backgroundColor: "rgb(47, 47, 47)", marginTop: "80px" }}>
         <h1 className='mb-5'>My Skills</h1>
-        <h4><FontAwesomeIcon className="mr-2" icon={faJsSquare}/> JavaScript</h4>
-        <h4><FontAwesomeIcon className="mr-2" icon={faReact}/> React</h4>
-        <h4><FontAwesomeIcon className="mr-2" icon={faHtml5}/> HTML5</h4>
-        <h4><FontAwesomeIcon className="mr-2" icon={faCss3Alt}/> CSS3</h4>
-        <h4><FontAwesomeIcon className="mr-2" icon={faGitAlt}/> Git</h4>
+        <h4 className="hvr-grow"><FontAwesomeIcon className="mr-2" icon={faJsSquare}/> JavaScript</h4> <br></br>
+        <h4 className="hvr-grow"><FontAwesomeIcon className="mr-2" icon={faReact}/>React</h4> <br></br>
+        <h4 className="hvr-grow"><FontAwesomeIcon className="mr-2" icon={faHtml5}/> HTML5</h4> <br></br>
+        <h4 className="hvr-grow"><FontAwesomeIcon className="mr-2" icon={faCss3Alt}/> CSS3</h4> <br></br>
+        <h4 className="hvr-grow"><FontAwesomeIcon className="mr-2" icon={faGitAlt}/>Git</h4>
         </Jumbotron>
     )
 }
