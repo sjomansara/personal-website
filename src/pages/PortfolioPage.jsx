c6be9b8deed1033ccb1bd6a1c9812d80e28d049f
sjomansara <sara.sjoman@hotmail.com>
2022-02-15 21:17:30 +0100

    card edit

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,58 +1,48 @@
-import { Col, Row, Card} from "react-bootstrap"
+import { Col, Row, Card } from "react-bootstrap"
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const PortfolioPage = () => {
     return (
+        <div>
+        <Jumbotron className="ml-5" style={{ backgroundColor: "rgb(47, 47, 47)", height: "5rem" }}>
+        <h1 className='ml-3'>My Portfolio</h1>
+        </Jumbotron>
         <div className="cardContainer">
-        <Row className="mx-5" style={{ marginTop: "80px" }}>
+        <Row className="mx-5">
             <Col md={3}>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                <Card className="portfolioCard" style={{ width: '18rem' }}>
+                    <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
+                            <Card.Title><a href="https://github.com/sjomansara/strive-capstoneproject-spotifystats">Spotify Stats App</a></Card.Title>
                         </Card.Body>
                 </Card>
             </Col>
             <Col md={3}>
-                <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                <Card className="portfolioCard" style={{ width: '18rem' }}>
+                        <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
                             <Card.Body>
                                 <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
                             </Card.Body>
                 </Card>
             </Col>
             <Col md={3}>
-                <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                <Card className="portfolioCard" style={{ width: '18rem' }}>
+                        <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
                             <Card.Body>
                                 <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
                             </Card.Body>
                     </Card>
             </Col>
             <Col md={3}>
-                <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                <Card className="portfolioCard" style={{ width: '18rem' }}>
+                        <Card.Img className="cardImg" variant="top" src="https://picsum.photos/100/70" />
                             <Card.Body>
                                 <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
                             </Card.Body>
                     </Card>
             </Col>
         </Row>
+        </div>
         </div>
     )
 }
