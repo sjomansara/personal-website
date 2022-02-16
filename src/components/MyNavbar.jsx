65db0bea4c246266ffbf63681bbbc2ef2933942d
sjomansara <sara.sjoman@hotmail.com>
2022-02-16 16:04:57 +0100

    fixed navbar hover animation

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -10,10 +10,10 @@ const MyNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-            <Link to="/"><Nav.Link href="#home" className="pr-3">Home</Nav.Link></Link>
-            <Link to="/aboutme"><Nav.Link href="#link" className="pr-3">About Me</Nav.Link></Link>
-            <Link to="/portfolio"><Nav.Link href="#link" className="pr-3">Portfolio</Nav.Link></Link>
-            <Nav.Link href="#link" className="me-3">Contact</Nav.Link>
+            <Link to="/"className="ms-3 hvr-grow">Home</Link>
+            <Link to="/aboutme" className="ms-3 hvr-grow">About Me</Link>
+            <Link to="/portfolio" className="ms-3 hvr-grow">Portfolio</Link>
+            <Link href="#link" className="ms-3 me-5 hvr-grow">Contact</Link>
         </Nav>
         </Navbar.Collapse>
         </Navbar>
