73da41c65e6b20d9d6dc7626166b2c4cf2fd77fd
sjomansara <sara.sjoman@hotmail.com>
2022-02-16 01:41:41 +0100

    added text to modal, changed card border radius

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,9 +1,11 @@
-import { Col, Row, Card, Container } from "react-bootstrap"
+import { Col, Row, Card, Container, Button } from "react-bootstrap"
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Modal from 'react-bootstrap/Modal';
 import { Carousel } from "react-bootstrap"
 import React from "react";
 import { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const PortfolioPage = () => {
     const [modalShow, setModalShow] = useState(false);
@@ -65,7 +67,7 @@ const PortfolioPage = () => {
               onHide={() => setModalShow(false)}
               aria-labelledby="example-modal-sizes-title-lg"
             >
-            <Modal.Header closeButton></Modal.Header>
+            <Modal.Header closeButton><h3 style={{ color: "#7D7D8C" }}>Spotify Stats App</h3></Modal.Header>
               <Modal.Body>
                 <Carousel>
                 <Carousel.Item>
@@ -90,7 +92,44 @@ const PortfolioPage = () => {
                     alt="Third slide"
                     />
                 </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="/stats4.png"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="/stats5.png"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="/stats6.png"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="/stats7.png"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="/stats8.png"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
                 </Carousel>
+                <p className="mt-4" style={{ color: "#7D7D8C" }}>Built as a capstone project for the Strive School frontend bootcamp, this app allows its users to see their most listened to artists and tracks, recently played tracks and recommended tracks by logging into their account using the Spotify API. The app was independently designed and developed on my own using React, Bootstrap, and CSS.</p>
+                <Button href="https://github.com/sjomansara/strive-capstoneproject-spotifystats" className="githubLink rounded-pill"><FontAwesomeIcon className="me-1" icon={faGithub}/> Open on GitHub</Button>
               </Modal.Body>
             </Modal>
             </div>
