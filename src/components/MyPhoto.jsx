600d5f375bdbfdb90f6a4ba100cce485c98d38f1
sjomansara <sara.sjoman@hotmail.com>
2022-02-17 14:39:35 +0100

    edited myphoto

--- src/components/MyPhoto.jsx
+++ src/components/MyPhoto.jsx
@@ -1,7 +1,9 @@
+import { Image } from "react-bootstrap"
+
 const MyPhoto = () => {
     return ( 
         // <img style={{ borderRadius: "50%", marginTop: "100px" }} src="https://avatars.githubusercontent.com/u/86473615?v=4" />
-        <img className="myPhoto mb-5" src="Untitled design (1).png" />
+        <Image fluid className="myPhoto mb-5 ms-5 me-5" src="Untitled design (1).png" />
     )
 }
 
